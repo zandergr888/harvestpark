7e6cec44b90e11ea338ed61c9b46f2a1b3066fa6
Alexander Hoang <alexanderhoang@Alexanders-MacBook-Pro-2.local>
2025-11-29 00:43:12 -0600

    added email to form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -64,7 +64,7 @@ export default function ContactForm() {
           <div className={styles.infoBox}>
             <h3>Stay Caffeinated!</h3>
             <p>
-              Have a comment or a question? Ready to schedule our coffee cart? Use the form below or email us and we can send you a customized quote - catering packages start at $695 for up to 50 people. Send us a message, and we will get back to you shortly.
+              Have a comment or a question? Ready to schedule our coffee cart? Use the form below or email us at <a href="mailto:catering@harvestpark.coffee">catering@harvestpark.coffee</a> and we can send you a customized quote - catering packages start at $695 for up to 50 people. Send us a message, and we will get back to you shortly.
             </p>
           </div>
 
